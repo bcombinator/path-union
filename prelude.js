2be3c4f74eeafdf29d217c36c89191ff7b3fdc79
David Chase <davidchase03@gmail.com>
2016-08-02 13:28:49 -0400

    feature(PRELUDE) simplify

--- prelude.js
+++ prelude.js
@@ -1,11 +1,25 @@
 'use strict'
 
-const compose = (f, g) => x => f(g(x))
+const _compose = function _compose(f, g) {
+    return function () {
+        return f(g.apply(null, arguments));
+    };
+};
+
+const compose = fns => fns.reduce(_compose)
 
 const uniq = xs => xs.reduce((list, x) => list.indexOf(x) === -1 ? list.concat(x) : list, [])
 
 const identity = x => x
 
-const flatten = xs => [].concat.apply([], xs.map(identity)); 
+const concat = (xs, list) => xs.concat(list)
 
-exports.union = compose(uniq, flatten)
+const union = compose([uniq, identity])
+
+const join = separator => xs => xs.join(separator)
+
+const split = separator => str => str.split(separator)
+
+const pathUnion = compose([join('/'), union, split('/'), concat])
+
+exports.pathUnion = pathUnion
