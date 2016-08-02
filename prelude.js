d8fe56410fecbe08430dec58513060b2c123d5f7
David Chase <davidchase03@gmail.com>
2016-08-02 14:08:57 -0400

    fix(PRELUDE) correct union sig
    the union signature was incorrect

--- prelude.js
+++ prelude.js
@@ -14,12 +14,18 @@ const identity = x => x
 
 const concat = (xs, list) => xs.concat(list)
 
-const union = compose([uniq, identity])
+const union = compose([uniq, concat])
 
 const join = separator => xs => xs.join(separator)
 
 const split = separator => str => str.split(separator)
 
-const pathUnion = compose([join('/'), union, split('/'), concat])
+const map = (fn, xs) => xs.map(fn)
+
+const chain = fn => xs => [].concat.apply([], map(fn, xs))
+
+const of = Array.of.bind(Array)
+
+const pathUnion = compose([join('/'), union, chain(split('/')), of])
 
 exports.pathUnion = pathUnion
